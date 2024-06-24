682b82b870c7bec3f30af4833228ed4dcbbb377e
ESTON NDUNGU <estonndungu1@gmail.com>
2024-06-24 14:04:44 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+
+// Declare a variable mostProfitableNeighborhood  and assign it a value of Chelsea
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Declare a variable companyCeo and assign it a value of Susan Smith
+let companyCeo = 'Susan Smith';
